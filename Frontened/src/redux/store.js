63dcf67a831f209d060be1cde1a5f88671e7ce23
MD ZEESHAN MUSTAFA <zeeshanmd2005@gmail.comL>
2026-09-08 17:39:53 +0530

    Initial commit

---
+++ Frontened/src/redux/store.js
@@ -0,0 +1,48 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
+import jobSlice from "./jobSlice";
+import companySlice from "./companySlice";
+import applicationSlice from "./applicationSlice";
+
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+
+import createWebStorage from 'redux-persist/es/storage/createWebStorage'
+
+const storage = createWebStorage('local')
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+}
+
+const rootReducer = combineReducers({
+    auth: authSlice,
+    job: jobSlice,
+    company: companySlice,
+    application: applicationSlice
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+
+export default store;
+export const persistor = persistStore(store);
