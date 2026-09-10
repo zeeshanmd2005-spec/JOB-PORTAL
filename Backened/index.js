62def045dd57259dcffc4f3e0719b2320cdd636f
MD ZEESHAN MUSTAFA <zeeshanmd2005@gmail.comL>
2026-09-10 16:29:43 +0530

    Fix production CORS

--- Backened/index.js
+++ Backened/index.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true
 };
 
